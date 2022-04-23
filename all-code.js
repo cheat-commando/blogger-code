fd46d5023eb559fc43a0dee13b987040e8549541
Carston Work <77954570+cheat-commando@users.noreply.github.com>
2022-04-22 20:30:11 -0600

    Update all-code.js

--- all-code.js
+++ all-code.js
@@ -1,10 +1,13 @@
 function sort_in_order() {
   var post_list = document.querySelector(".blog-posts.hfeed.container");
+  console.log(post_list);
   var these_children = postlist.children;
+  console.log(these_children);
   var resort_list = [];
   for (let i = these_children.length; i > 0; i--) {
       resort_list.push(post_list[i]);
     }
+  console.log(resort_list);
   post_list.innerHTML = "";
   for (let i = 0; i < resort_list.length; i++) {
       post_list.appendChild(resort_list[i]);
